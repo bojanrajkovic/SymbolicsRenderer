d4a15aa9f05a4bc2765266f794815156c23276f5
Bojan Rajkovic <brajkovic@xamarin.com>
2018-02-09 13:19:27 -0500

    Lazily load MathNet types to improve consistency.

--- src/SymbolicsRenderer/SymbolicsIntegration.cs
+++ src/SymbolicsRenderer/SymbolicsIntegration.cs
@@ -16,29 +16,24 @@ namespace SymbolicsRenderer
         const string Tag = nameof(SymbolicsIntegration);
         const string ExpressionTypeName = "MathNet.Symbolics.Expression, MathNet.Symbolics, Culture=neutral, PublicKeyToken=null";
         static Type SymbolicsExpressionType;
-
-        // Use the FQTN to load the type. The renderer uses reflection because MathNet.Symbolics
-        // is a .NET 4.0-targeting library, so we can't get a direct reference into a .NET Standard library.
-        static SymbolicsIntegration() =>
-            SymbolicsExpressionType = Type.GetType(ExpressionTypeName);
         
         public void IntegrateWith(IAgent agent)
         {
-            if (SymbolicsExpressionType == null) {
-                Log.Warning(
-                    Tag,
-                    "Cannot initialize MathNet.Symbolics representation provider! " +
-                    "Could not load MathNet.Symbolics.Expression type."
-                );
-                return;
-            }
-
             Log.Debug(Tag, "Registering MathNet.Symbolics representation provider!");
             agent.RepresentationManager.AddProvider(this);
         }
 
         public override IEnumerable<object> ProvideRepresentations(object obj)
         {
+            if (SymbolicsExpressionType == null) {
+                try {
+                    SymbolicsExpressionType = Type.GetType(ExpressionTypeName, throwOnError: true);
+                } catch (Exception e) {
+                    Log.Warning(Tag, e, "Could not load MathNet.Symbolics.Expression type.");
+                    yield break;
+                }
+            }
+
             var objectType = obj.GetType();
             if (SymbolicsExpressionType.IsAssignableFrom(objectType))
                 yield return new SymbolicExpressionWrapper(obj);
